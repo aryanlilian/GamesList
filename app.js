c725777d30012297423bd97e2cd28b66a424a17c
Bularu Lilian <aryanlilian@gmail.com>
2021-06-01 19:59:55 +0300

    configured the view engine and directories

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const morgan = require('morgan');
 const indexRoutes = require('./routes/index');
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use('/', indexRoutes);
+app.use((req, res) => {
+    res.status(404).render('404');
+})
 app.listen(8000, () => console.log('Server is listening on port 8000...!'));
